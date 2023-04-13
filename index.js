52e9032f9a3c4442f2c1cf261a6cb047ba967824
alvinkenyagah <alvin47k@gmail.com>
2023-04-13 17:13:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = new Set(); // initialize an empty Set
+  for (const number of array) {
+    const complement = target - number;
+
+    // .has returns true if the Set includes the complement
+    if (seenNumbers.has(complement)) return true;
+
+    // .add adds the number to the Set
+    seenNumbers.add(number);
+  }
+  return false;
 }
+
+
+
+hasTargetSum([3, 8, 12, 4, 11, 7], 10);
+// returns true, since 3 and 7 add up to 10
+
+hasTargetSum([22, 19, 4, 6, 30], 25);
+// returns true, since 19 and 6 add up to 25
+
+hasTargetSum([1, 2, 5], 4);
+// returns false, since no pair of numbers adds up to 4
+
+
+
+
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,7 +36,14 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
-*/
+// */
+// iterate over the array of numbers
+//   for the current number, identify a complementary number that adds to our target
+//   (for example: if our number is 2, and the target is 5, the complementary number is 3)
+//   iterate over the remaining numbers in the array
+//     check if any of the remaining numbers is the complement
+//       if so, return true
+// if we reach the end of the array, return false
 
 /*
   Add written explanation of your solution here
